0f8dda658d80493fd5a3c2771662921202a4dfb2
yohann <yohann@thinkr.fr>
2024-07-24 11:16:51 +0200

    step3

--- inst/app/www/barchartJS.js
+++ inst/app/www/barchartJS.js
@@ -3,25 +3,37 @@ $(document).ready(function () {
     console.log(arg);
     const ctx = document.getElementById("myChart");
 
-    new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: arg.labels,
-        datasets: [
-          {
-            label: arg.label,
-            data: arg.data,
-            borderWidth: arg.borderWidth,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
+    const chart = Chart.getChart("myChart");
+
+    if (chart == undefined) {
+      console.log("Creating a new chart");
+      new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: arg.labels,
+          datasets: [
+            {
+              label: arg.label,
+              data: arg.data,
+              borderWidth: arg.borderWidth,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
           },
         },
-      },
-    });
+      });
+    } else {
+      console.log("Updating an existing chart");
+      chart.data.labels = arg.labels;
+      chart.data.datasets[0].label = arg.label;
+      chart.data.datasets[0].data = arg.data;
+      chart.data.datasets[0].borderWidth = arg.borderWidth;
+      chart.update();
+    }
   });
 });
