d47ce3936f55d604fc39509ca51de7f842644a41
yohann <yohann@thinkr.fr>
2024-07-24 15:09:15 +0200

    step8

--- inst/app/www/scatterplotJS.js
+++ inst/app/www/scatterplotJS.js
@@ -4,48 +4,60 @@ $(document).ready(function () {
 
     console.log(arg);
 
-    const data = {
-      datasets: [
-        {
-          label: arg.label,
-          data: arg.data,
-          borderColor: arg.pointBorderColor,
-          backgroundColor: arg.pointBackGroundColor,
+    const chart2 = Chart.getChart("myChart2");
+
+    if (chart2 == undefined) {
+      console.log("Creating a new chart");
+
+      const data = {
+        datasets: [
+          {
+            label: arg.label,
+            data: arg.data,
+            borderColor: arg.pointBorderColor,
+            backgroundColor: arg.pointBackGroundColor,
+          },
+        ],
+      };
+
+      const plugins = {
+        title: {
+          display: true,
+          text: arg.mainTitle,
+          color: arg.mainTitleColor,
         },
-      ],
-    };
+      };
 
-    const plugins = {
-      title: {
-        display: true,
-        text: arg.mainTitle,
-        color: arg.mainTitleColor,
-      },
-    };
-
-    const config = {
-      type: "scatter",
-      data: data,
-      options: {
-        plugins: plugins,
-        scales: {
-          x: {
-            type: "linear",
-            position: "bottom",
-            title: {
-              display: true,
-              text: arg.xAxisTitle,
+      const config = {
+        type: "scatter",
+        data: data,
+        options: {
+          plugins: plugins,
+          scales: {
+            x: {
+              type: "linear",
+              position: "bottom",
+              title: {
+                display: true,
+                text: arg.xAxisTitle,
+              },
+            },
+            y: {
+              title: {
+                display: true,
+                text: arg.yAxisTitle,
+              },
             },
           },
-          y: {
-            title: {
-              display: true,
-              text: arg.yAxisTitle,
-            },
-          },
         },
-      },
-    };
-    new Chart(ctx, config);
+      };
+      new Chart(ctx, config);
+    } else {
+      console.log("Updating an existing chart");
+      chart2.data.datasets[0].backgroundColor = arg.pointBackGroundColor;
+      chart2.options.plugins.title.text = arg.mainTitle;
+      chart2.options.plugins.title.color = arg.mainTitleColor;
+      chart2.update();
+    }
   });
 });
